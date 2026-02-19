828fe8a6bb670becbd2c9f91cd972ac776c27771
warpersan <leumas.ecole@gmail.com>
2026-02-19 17:25:48 -0500

    Added basic loading of file

--- source/Plateau.cpp
+++ source/Plateau.cpp
@@ -1,11 +1,43 @@
 #include "../include/Plateau.h"
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include "../include/string_utils.h"
+
+#define COMMENT_CHAR '#'
+
 Plateau::Plateau()
 {
 }
 
 bool Plateau::charger(const std::string& fichier)
 {
+	if (!std::filesystem::exists(fichier))
+	{
+		std::cerr << "File does not exist at '" << fichier << "'." << std::endl;
+		return false;
+	}
+
+	std::ifstream stream(fichier);
+	
+	std::string line;
+	while (std::getline(stream, line))
+	{
+		line = trim(line);
+		
+		if (line.empty())
+			continue;
+		
+		if (line.at(0) == COMMENT_CHAR)
+			continue;
+
+		std::cout << line << std::endl;
+	}
+	
+	stream.close();
+	return true;
 }
 
 void Plateau::menu()
