0d368c693927321aefe3191580d6f8d6c5752203
warpersan <leumas.ecole@gmail.com>
2026-02-19 20:41:07 -0500

    Implemented `Ville::totalCubes()` and `Ville::cubesDe()`

--- include/Ville.h
+++ include/Ville.h
@@ -4,20 +4,26 @@
 #include <string>
 #include <vector>
 
-class Ville {
+class Ville
+{
 public:
 	/// Name of the city
 	std::string nom;
-	
+
 	/// Color of the city
 	std::string couleur;
 	bool port;
-	std::map<std::string, int> cubes;
 	std::vector<std::string> voisins;
 
 	Ville();
 	Ville(const std::string& nom, const std::string& couleur, bool port);
 
+	/// Cubes accumulated by color on the city
+	std::map<std::string, int> cubes;
+
+	/// Gets the amount of cubes stored on the city
 	[[nodiscard]] int totalCubes() const;
+
+	/// Gets the amount of cubes of a given color
 	[[nodiscard]] int cubesDe(const std::string& couleur) const;
 };
--- source/Ville.cpp
+++ source/Ville.cpp
@@ -1,5 +1,7 @@
 #include "../include/Ville.h"
 
+#include <ranges>
+
 Ville::Ville()
 {
 }
@@ -9,4 +11,22 @@ Ville::Ville(const std::string& nom, const std::string& couleur, bool port)
 	this->nom = nom;
 	this->couleur = couleur;
 	this->port = port;
+}
+
+int Ville::totalCubes() const
+{
+	int total = 0;
+
+	for (const auto count : this->cubes | std::views::values)
+		total += count;
+
+	return total;
+}
+
+int Ville::cubesDe(const std::string& couleur) const
+{
+	if (this->cubes.contains(couleur))
+		return this->cubes.at(couleur);
+	
+	return 0;
 }
