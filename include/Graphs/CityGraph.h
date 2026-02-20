99a2ff73cd813b12796d4bdc347a2f2067d4a05b
warpersan <leumas.ecole@gmail.com>
2026-02-20 14:03:18 -0500

    Changed `CityGraph` to inherit `BaseGraph`

--- include/Graphs/CityGraph.h
+++ include/Graphs/CityGraph.h
@@ -1,9 +1,10 @@
 #ifndef INC_8INF259_TP2_CITYGRAPH_H
 #define INC_8INF259_TP2_CITYGRAPH_H
-#include "../Graph.h"
+#include "BaseGraph.h"
+#include "../Ville.h"
 
 /// Graph that represents a network of cities
-class CityGraph : public Graph
+class CityGraph : public BaseGraph
 {
 public:
 	/// Loads the graph from the given stream
