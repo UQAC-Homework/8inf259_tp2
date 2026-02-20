9d07d7acd37774f57b91fec646ef6ed5381cee12
warpersan <leumas.ecole@gmail.com>
2026-02-20 13:48:41 -0500

    Added class `BaseGraph`

---
+++ include/Graphs/BaseGraph.h
@@ -0,0 +1,10 @@
+#ifndef INC_8INF259_TP2_BASEGRAPH_H
+#define INC_8INF259_TP2_BASEGRAPH_H
+
+/// Class that represents a graph of any kind
+class BaseGraph
+{
+};
+
+
+#endif //INC_8INF259_TP2_BASEGRAPH_H
