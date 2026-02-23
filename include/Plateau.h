64ea138fe3bef6837520c3b94133707fd243f1f2
warpersan <leumas.ecole@gmail.com>
2026-02-23 17:10:21 -0500

    Format

--- include/Plateau.h
+++ include/Plateau.h
@@ -9,12 +9,13 @@ class Plateau
 public:
 	Plateau();
 
-	/// Loads the map from the file
 	bool charger(const std::string& fichier);
+
 	void actionPlacerTroisCubes();
 	void actionInfecter();
 	void actionPlacerRail();
 	void actionPlusCourtChemin();
 	void afficherEtat();
+
 	void menu();
 };
