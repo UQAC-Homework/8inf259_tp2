4558c8eb469523d8c5c9e420d612b898a1e234bf
WarperSan <leumas.ecole@gmail.com>
2026-03-08 09:24:06 -0400

    Updated `utils::graph`

--- include/utils/graph.h
+++ include/utils/graph.h
@@ -2,20 +2,19 @@
 #define INC_8INF259_TP2_GRAPH_UTILS_H
 #include <cassert>
 #include <iosfwd>
-#include <vector>
 
-#include "string.h"
 #include "../CityGraph.h"
+#include "../utils/string.h"
 
 namespace utils::graph
 {
 	/// Loads the graph from the given stream
 	inline CityGraph loadGraphFromStream(std::istream& stream)
 	{
-		#define COMMENT_CHAR '#'
-		#define DIVIDER_CHAR ';'
-		#define CITY_START "VILLES"
-		#define LINKS_START "LIENS"
+#define COMMENT_CHAR '#'
+#define DIVIDER_CHAR ';'
+#define CITY_START "VILLES"
+#define LINKS_START "LIENS"
 
 		CityGraph graph;
 
@@ -54,27 +53,27 @@ namespace utils::graph
 
 			if (processing_cities)
 			{
-				const std::vector<std::string> tokens = string::split(line, DIVIDER_CHAR);
+				const auto tokens = string::split(line, DIVIDER_CHAR);
 
 				if (tokens.size() < 3)
 					throw std::out_of_range("Line has missing tokens: " + line);
 
-				const std::string& name = tokens.at(0);
-				const std::string& color = tokens.at(1);
-				const std::string& is_port = tokens.at(2);
+				const auto& name = tokens.at(0);
+				const auto& color = tokens.at(1);
+				const auto& is_port = tokens.at(2);
 
 				const Ville city(name, color, is_port == "1");
 				graph.addCity(city);
 			}
 			else
 			{
-				const std::vector<std::string> tokens = string::split(line, DIVIDER_CHAR);
+				const auto tokens = string::split(line, DIVIDER_CHAR);
 
 				if (tokens.size() < 2)
 					throw std::out_of_range("Line has missing tokens: " + line);
 
-				const std::string& origin = tokens.at(0);
-				const std::string& destination = tokens.at(1);
+				const auto& origin = tokens.at(0);
+				const auto& destination = tokens.at(1);
 
 				graph.addRoad(origin, destination);
 			}
